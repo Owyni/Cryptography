bc8b9e14220063aae76512dfd572fa54291377ec
Owynn <angelaleman2212@gmail.com>
2025-08-22 12:10:46 -0600

    Little fix

--- GCD/greatestCommonDivisor.py
+++ GCD/greatestCommonDivisor.py
@@ -1,8 +1,7 @@
-# Solicita dos números al usuario
 a = int(input("Primer número: "))
 b = int(input("Segundo número: "))
 
-# Calcula el máximo común divisor usando el algoritmo de Euclides paso a paso
+# Algoritmo de Euclides
 while b != 0:
     temp = b
     b = a % b
